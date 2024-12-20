0b41578ac48acf98d8ab42b168b9bdb6466136af
GyeomFka <clazziquai01@gmail.com>
2024-12-20 15:42:00 +0900

    [add] request를 받아 요청 경로를 반환해주는 StringUtil 클래스

---
+++ src/util/StringUtil.java
@@ -0,0 +1,7 @@
+package util;
+
+public class StringUtil {
+    public String getRequestStartLine(String requestInfo) {
+        return requestInfo.split(System.lineSeparator(), 2)[0].split("\\s+", 3)[1];
+    }
+}
