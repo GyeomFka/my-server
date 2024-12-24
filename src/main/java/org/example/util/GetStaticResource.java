fa2eba1a5e4808ce090f02e1e5976fc5f3882b5d
GyeomFka <clazziquai01@gmail.com>
2024-12-24 10:49:06 +0900

    [update] util 클래스 정적자원활용

--- src/main/java/org/example/util/GetStaticResource.java
+++ src/main/java/org/example/util/GetStaticResource.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class GetStaticResource {
-    public byte[] getHtmlInfo(String fileName) throws IOException {
+    public static byte[] getHtmlInfo(String fileName) throws IOException {
         return Files.readAllBytes(new File("./webapp/" + fileName + ".html").toPath());
     }
 }
--- src/main/java/org/example/util/StringUtil.java
+++ src/main/java/org/example/util/StringUtil.java
@@ -1,7 +1,12 @@
 package org.example.util;
 
 public class StringUtil {
-    public String getRequestStartLine(String requestInfo) {
+    public static String getRequestStartLine(String requestInfo) {
         return requestInfo.split(System.lineSeparator(), 2)[0].split("\\s+", 3)[1];
+    }
+
+    public static boolean isValidQueryString(String requestUrl) {
+        return requestUrl.contains("?")
+                && requestUrl.indexOf("?") < requestUrl.length() - 1;
     }
 }
