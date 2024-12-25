1bda837f49a2a46c8c4787e453ad18cbc3a3bbbc
GyeomFka <clazziquai01@gmail.com>
2024-12-25 23:05:24 +0900

    [refactoring] RequestHandler에서 Request클래스, Response클래스를 분리

---
+++ src/main/java/org/example/webserver/HttpRequest.java
@@ -0,0 +1,82 @@
+package org.example.webserver;
+
+import org.example.util.HttpRequestUtils;
+import org.example.util.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.namespace.QName;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
+
+    private String method;
+    private String path;
+    private Map<String, String> headers = new HashMap<String, String>();
+    private Map<String, String> params = new HashMap<String, String>();
+
+    public HttpRequest(InputStream in) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+
+            processRequestLine(line);
+
+            line = br.readLine();
+            while (!line.equals("")) {
+                logger.info("header={}", line);
+                String[] tokens = line.split(":");
+                headers.put(tokens[0].trim(), tokens[1].trim());
+                line = br.readLine();
+            }
+
+            if ("POST".equals(method)) {
+                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                params = HttpRequestUtils.parseQueryString(body);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void processRequestLine(String requestLine) {
+        logger.info("request line={}", requestLine);
+        String[] tokens = requestLine.split(" ");
+        method = tokens[0];
+
+        if ("POST".equals(method)) {
+            path = tokens[1];
+            return;
+        }
+
+        int index = tokens[1].indexOf("?");
+        if (index == -1) {
+            path = tokens[1];
+        } else {
+            path = tokens[1].substring(0, index);
+            params = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
+        }
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String Path() {
+        return path;
+    }
+
+    public String getHeader(String name) {
+        return headers.get(name);
+    }
+
+    public String getParameter(String name) {
+        return params.get(name);
+    }
+}
---
+++ src/main/java/org/example/webserver/HttpResponse.java
@@ -0,0 +1,4 @@
+package org.example.webserver;
+
+public class HttpResponse {
+}
